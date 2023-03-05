00d11c7530f85784c6a717f5504b2df1afbeb4d7
ray491 <72789534+ray491@users.noreply.github.com>
2023-03-05 09:19:43 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
  const userInput = prompt("Wachtwoord: ")
  // omar
   if (userInput.toLowerCase() === "1223223434554322") {
-        window.open("/files/omar.html");
+        window.open("Raygo-imp-bank/files/omar.html");
       }
 // roya
        if (userInput.toLowerCase() === "1122332233445") {
-        window.open("/files/roya.html");
+        window.open("Raygo-imp-bank/files/roya.html");
       }
 // root
        if (userInput.toLowerCase() === "root") {
-        window.location.href("/files/admin.html");
+        window.open("Raygo-imp-bank/files/admin.html");
       }
